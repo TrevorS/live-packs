fd9d4248a5bc9c38c49370b91593641a0b4ff3e9
Trevor Strieber <trevor@strieber.org>
2012-10-29 19:10:44 -0400

    initial commit

---
+++ adefa-pack/config/bindings.el
@@ -0,0 +1,9 @@
+;; Place your bindings here.
+
+;; For example:
+;;(define-key global-map (kbd "C-+") 'text-scale-increase)
+;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+;; Set key binding for ace-jump-mode
+;;(global-set-key "\M-j" 'ace-jump-mode)
+(define-key global-map (kbd "M-j") 'ace-jump-mode)
