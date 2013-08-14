11511f9fb83cc7a031225ae504521fd21838cc35
Trevor Strieber <trevor@strieber.org>
2013-08-14 14:39:53 -0400

    adding go-mode require

--- go-pack/init.el
+++ go-pack/init.el
@@ -3,5 +3,9 @@
 ;; User this file to initiate the pack configuration.
 ;; See README for more information.
 
+;; Load go-mode
+(live-add-pack-lib "go-mode")
+(require 'go-mode)
+
 ;; Load go-pack settings
 (live-load-config-file "settings.el")
