7d74ec2db30262034f5ad2c8d84e789c8581f2b8
Trevor Strieber <trevor@strieber.org>
2013-08-14 14:35:25 -0400

    creating go-pack

---
+++ go-pack/init.el
@@ -0,0 +1,7 @@
+;; User pack init file
+;;
+;; User this file to initiate the pack configuration.
+;; See README for more information.
+
+;; Load go-pack settings
+(live-load-config-file "settings.el")
