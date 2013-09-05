6e4bd5abd27fbf7f240ee57e6f09f2145fc77d0f
Trevor Strieber <trevor@strieber.org>
2013-09-05 10:37:00 -0600

    Updating emacs-live.el to load the go-pack.

--- etc/emacs-live.el
+++ etc/emacs-live.el
@@ -1,4 +1,7 @@
+;; stop loading emacs-live default keybindings
+(live-ignore-packs '(live/bindings-pack))
+
+;; add in the live-packs
 (live-add-packs '(~/.live-packs/trevor-pack))
 (live-add-packs '(~/.live-packs/go-pack))
 (live-add-packs '(~/.live-packs/rails-pack))
-(live-ignore-packs '(live/bindings-pack))
